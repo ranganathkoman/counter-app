b8f606b2be7b3b8eb485826cac774c12e1b00ac2
K Ranganath <ranganath@rudderstack.com>
2022-09-01 15:31:14 +0530

    Updates

---
+++ precache-manifest.4a832a2187029c0d4b953274082781a2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "683703f50643b7f49f36",
+    "url": "/counter-app/static/css/main.bbc0c4af.chunk.css"
+  },
+  {
+    "revision": "683703f50643b7f49f36",
+    "url": "/counter-app/static/js/main.383cf069.chunk.js"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "9ba0f491065f629a9d1f",
+    "url": "/counter-app/static/css/2.4dd06c70.chunk.css"
+  },
+  {
+    "revision": "9ba0f491065f629a9d1f",
+    "url": "/counter-app/static/js/2.41ae138d.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3265719f7f58980882e576f4cde13280",
+    "url": "/counter-app/index.html"
+  }
+];
