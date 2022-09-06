fea810170487372630e67386252e882978f80abd
K Ranganath <ranganath@rudderstack.com>
2022-09-06 20:15:55 +0530

    Updates

--- precache-manifest.402736bc519c461caa040ad9dcd349e0.js
+++ precache-manifest.402736bc519c461caa040ad9dcd349e0.js
@@ -1,46 +1,46 @@
 self.__precacheManifest = [
   {
-    "revision": "683703f50643b7f49f36",
+    "revision": "c1471a8611e331e750dc",
     "url": "/counter-app/static/css/main.bbc0c4af.chunk.css"
   },
   {
-    "revision": "683703f50643b7f49f36",
-    "url": "/counter-app/static/js/main.383cf069.chunk.js"
+    "revision": "c1471a8611e331e750dc",
+    "url": "/counter-app/static/js/main.2dd851f1.chunk.js"
   },
   {
     "revision": "0f1f037b32656fd93884",
     "url": "/counter-app/static/js/runtime~main.c63f0888.js"
   },
   {
-    "revision": "9ba0f491065f629a9d1f",
+    "revision": "308f2150ba5e46087008",
     "url": "/counter-app/static/css/2.4dd06c70.chunk.css"
   },
   {
-    "revision": "9ba0f491065f629a9d1f",
-    "url": "/counter-app/static/js/2.41ae138d.chunk.js"
-  },
-  {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+    "revision": "308f2150ba5e46087008",
+    "url": "/counter-app/static/js/2.014b2c1d.chunk.js"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
-  },
-  {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "3265719f7f58980882e576f4cde13280",
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "d44bbdac7a659de256fdef5d04558c49",
     "url": "/counter-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/counter-app/precache-manifest.45e05e2d3ebfbe72f19ae5ce262489a0.js"
+  "/counter-app/precache-manifest.402736bc519c461caa040ad9dcd349e0.js"
 );
 
 workbox.clientsClaim();
