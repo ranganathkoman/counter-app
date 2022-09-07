84f1a613956f33d09d1db8f69cc603b96fe1e869
K Ranganath <ranganath@rudderstack.com>
2022-09-07 18:08:15 +0530

    Remove the GA4

--- src/index.js
+++ src/index.js
@@ -5,24 +5,8 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import ReactGA from "react-ga4";
-
 
 // ReactDOM.render(<App />, document.getElementById("root"));
-
-ReactGA.initialize([
-    {
-      trackingId: "your GA measurement id",
-      gaOptions: {}, // optional
-      gtagOptions: {config: {
-        page_title: 'Travel Destinations',
-        currency: 'USD', 
-        floor: "10 th FLoor",
-        building: "My Home Vihanga",
-        flat: 2015
-      }}, // optional
-    },
-  ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
