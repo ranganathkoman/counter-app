29bb067a0ed7a139d88c3d1f7d1376ad15569fa9
K Ranganath <ranganath@rudderstack.com>
2022-09-07 15:03:59 +0530

    Add the gtag config file

---
+++ src/rudder/ga.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-G7HS7HSRP6', {
+  'page_title': 'Travel Destinations',
+  'currency': 'USD',
+  'floor': "20th floor",
+  'flat': "2013"
+});
