0e210b73ff62d8856ebe1b970de08562582ce1cf
K Ranganath <ranganath@rudderstack.com>
2022-09-07 15:44:46 +0530

    Using the react-ga4 library

--- src/index.js
+++ src/index.js
@@ -5,11 +5,24 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import gtagFunc from "./rudder/ga";
+import ReactGA from "react-ga4";
+
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
-gtagFunc();
+ReactGA.initialize([
+    {
+      trackingId: "your GA measurement id",
+      gaOptions: {}, // optional
+      gtagOptions: {config: {
+        page_title: 'Travel Destinations',
+        currency: 'USD', 
+        floor: "10 th FLoor",
+        building: "My Home Vihanga",
+        flat: 2015
+      }}, // optional
+    },
+  ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
